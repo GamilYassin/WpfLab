8523d83cbfdcaab3d136462ed3f5843e8e27dc66
Gamil Yassin <gamil.yassin@gmail.com>
2020-09-10 01:37:12 +0300

    ss

--- WpfLab/MainWindow.xaml.cs
+++ WpfLab/MainWindow.xaml.cs
@@ -15,19 +15,36 @@ using System.Windows.Shapes;
 
 namespace WpfLab
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructors
 
-		private void Border_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
-		{
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
 
-		}
-	}
+        #endregion Constructors
+
+        #region Methods
+
+        private void Border_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
+        {
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("First Button Clicked");
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Second Button Clicked");
+        }
+
+        #endregion Methods
+    }
 }
